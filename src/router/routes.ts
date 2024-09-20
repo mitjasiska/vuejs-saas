7164e313dc0b76a9dc79cd3bf2346b44f48b83de
Mitja Šiška <mitja.siska@gmail.com>
2024-09-20 20:02:06 +0200

    Add login and register pages with updated routing.
    Added new LoginPage.vue and RegisterPage.vue components. Updated the routing configuration to include paths for login and register pages, relocating existing route definitions to a separate routes file.

---
+++ src/router/routes.ts
@@ -0,0 +1,30 @@
+import type { RouteRecordRaw } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: import('@/views/LoginPage.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: import('@/views/RegisterPage.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue')
+  }
+];
+
+export default routes;
---
+++ src/views/RegisterPage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>Register page</div>
+</template>
+
+<style scoped>
+
+</style>
